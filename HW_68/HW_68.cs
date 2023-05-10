3ad299cc8f6a172c730b3fba3488abff9344d537
Olga Dobrydina <doa1981@mail.ru>
2023-05-10 20:18:06 +0500

    AckermanFunction

--- HW_68/HW_68.cs
+++ HW_68/HW_68.cs
@@ -2,3 +2,18 @@
 Даны два неотрицательных числа m и n.
 m = 2, n = 3 -> A(m,n) = 9
 m = 3, n = 2 -> A(m,n) = 29 */
+
+Console.WriteLine("Введите n ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите m ");
+int m = int.Parse(Console.ReadLine());
+AckermanFunction(n, m);
+Console.Write(AckermanFunction(n, m));
+
+
+int AckermanFunction(int n,int m)
+{
+    if (n == 0) return m + 1;
+    else if (m == 0) return AckermanFunction(n - 1, 1);
+    else return AckermanFunction(n -1, AckermanFunction(n, m - 1));
+}
